099fc30fa23daa77d99f2140cdd79e62f76bc88c
studymadara <virajwagh6@gmail.com>
2020-10-08 21:56:44 +0530

    thymeleaf added username and email id will be displayed.

--- src/main/java/com/integration/bundle/BundleApplication.java
+++ src/main/java/com/integration/bundle/BundleApplication.java
@@ -2,6 +2,7 @@ package com.integration.bundle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @SpringBootApplication
 public class BundleApplication {
--- src/main/java/com/integration/bundle/controller/MainController.java
+++ src/main/java/com/integration/bundle/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.integration.bundle.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController 
@@ -12,15 +16,19 @@ public class MainController
 	//3.in another page give logout button after that back to step 1
 	
 	@GetMapping("/loginPage")
-	public String showPage()
+	public ModelAndView showPage()
 	{
-		return "loginPage.html";
-	}
-	
-	@GetMapping("/shadyPage")
-	public String shadyPage()
-	{
-		return "shadyPage.html";
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("loginPage");
+		return modelAndView;
 	}
 
+	@GetMapping("/shadyPage")
+	public ModelAndView shadyPage(@AuthenticationPrincipal OAuth2User user){
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("finalShadyPage");
+		modelAndView.addObject("username",user.getAttributes().get("name"));
+		modelAndView.addObject("emailId",user.getAttributes().get("email"));
+		return modelAndView;
+	}
 }
